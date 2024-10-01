22916b2d7722da79a5013d739c68820c42091b1b
alexelmer123 <1552557@senati.pe>
2024-10-01 14:10:28 -0500

    1

--- JavaSwinApp/src/com/alex/pe/cdb/Conexion.java
+++ JavaSwinApp/src/com/alex/pe/cdb/Conexion.java
@@ -2,70 +2,41 @@
 package com.alex.pe.cdb;
 
 import java.sql.Connection;
-
 import java.sql.DriverManager;
 
 public class Conexion {
+    
+    private static Connection con = null;
+    private static String usuario = "TIENDA_VIRTUAL1";
+    private static String contraseña = "1234560";
+    private static String url = "jdbc:oracle:thin:@localhost:1521:XE";
 
- 
-
-  private static Connection con = null;
-
-  private static String usuario = "TIENDA_VIRTUAL1";
-
-  private static String contraseña = "1234560";
-
-  private static String url = "jdbc:oracle:thin:@localhost:1521:XE";
-
-   
-
-  public static Connection getConnection(){
+   public static Connection getConnection(){
 
     try {
-
-      //cargar el controlador jdvc
+//cargar el controlador jdvc
 
       Class.forName("oracle.jdbc.OracleDriver");
 
       //establecer la conexion con la base de datos 
 
       con = DriverManager.getConnection(url, usuario, contraseña);
-
       con.setAutoCommit(false);
-
-       
-
+      
       System.out.println("alex");
-
-       
-
-       
-
+      
       if(con != null){
 
         System.out.println("se a conectado");
-
       }else {
-
-        System.out.println("error conexion fallida");
-
+          System.out.println("error conexion fallida");
       }
-
     } catch (Exception e) {
-
         System.out.println("Error, no se pudo desconectar"+e.getMessage());
-
-
-
-    }
-
+}
     return con;
-
-  
-
-  };
-
-  public void closeConexion(){
+};
+   public void closeConexion(){
 
     try {
 
@@ -87,3 +58,5 @@ public class Conexion {
   }
 
 }
+
+    
